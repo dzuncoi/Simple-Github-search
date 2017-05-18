25b55382c1add7a8d0afe33e1853d5e91d14bba2
Dung Huynh <clock.quangdung@gmail.com>
2017-05-19 01:44:22 +0700

    integrate basic component for Searchbox and SearchItem

--- src/App.js
+++ src/App.js
@@ -1,18 +1,104 @@
+/*
+* @author: dzuncoi
+* @date: May 19 2015
+* @version: 0.0.1
+*/
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  SearchBox
+} from 'office-ui-fabric-react/lib/SearchBox';
+import SearchItem from './components/SearchItem';
+
+const users = [{
+  avatar_url: "https://avatars0.githubusercontent.com/u/59207?v=3",
+  login: "tomster"
+}, {
+  avatar_url: "https://avatars3.githubusercontent.com/u/2454540?v=3",
+  login: "TomBZombie"
+},{
+  avatar_url: "https://avatars0.githubusercontent.com/u/59207?v=3",
+  login: "tomster"
+}, {
+  avatar_url: "https://avatars3.githubusercontent.com/u/2454540?v=3",
+  login: "TomBZombie"
+},{
+  avatar_url: "https://avatars0.githubusercontent.com/u/59207?v=3",
+  login: "tomster"
+}, {
+  avatar_url: "https://avatars3.githubusercontent.com/u/2454540?v=3",
+  login: "TomBZombie"
+}, {
+  avatar_url: "https://avatars3.githubusercontent.com/u/2454540?v=3",
+  login: "TomBZombie"
+},{
+  avatar_url: "https://avatars0.githubusercontent.com/u/59207?v=3",
+  login: "tomster"
+}, {
+  avatar_url: "https://avatars3.githubusercontent.com/u/2454540?v=3",
+  login: "TomBZombie"
+}]
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: ''
+    }
+    this.onSearchBoxChanged = this.onSearchBoxChanged.bind(this);
+    this.onSelectUser = this.onSelectUser.bind(this);
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+  }
+
+  onSearchBoxChanged(value) {
+    this.setState({
+      searchText: value
+    });
+  }
+
+  onSelectUser(user) {
+    console.log(user);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <p className="ms-font-xxl ms-fontWeight-regular">GITHUB USER SEARCH</p>
         </div>
-        <p className="App-intro">
-          My app
-        </p>
+        <div className="search-container">
+          <div className="search-wrapper">
+            <SearchBox
+              labelText="Search by username ..."
+              value={this.state.searchText}
+              onChange={this.onSearchBoxChanged}
+              />
+            <div className="search-items">
+              {
+                users.map(user => (
+                  <SearchItem
+                    key={Math.random()}
+                    user={user}
+                    onSelect={() => this.onSelectUser(user)}/>
+                ))
+              }
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/SearchItem.js
@@ -0,0 +1,26 @@
+/*
+* @author: dzuncoi
+* @date: May 19 2015
+* @version: 0.0.1
+*/
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './SearchItem.css';
+
+const SearchItem = props => {
+  const { user, onSelect } = props;
+  return (
+    <div onClick={onSelect} className="search-item">
+      <img src={user.avatar_url} alt="User's Avatar"/>
+      <p className="ms-font-l ms-fontWeight-regular">{user.login}</p>
+    </div>
+  )
+}
+
+SearchItem.propTypes = {
+  user: PropTypes.object.isRequired,
+  onSelect: PropTypes.func
+}
+
+export default SearchItem
