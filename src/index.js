d5226e65bf6801b77aeb6e1541d7693bcd8154b0
Dung Huynh <clock.quangdung@gmail.com>
2017-05-18 23:38:13 +0700

    setup Redux store, reducers and React router

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Router, Route, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const store = createStore(
+  reducers,
+  // This one is used for Redux DevTools extension
+  // You can find it out at https://github.com/zalmoxisus/redux-devtools-extension
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk),
+)
+
+ReactDOM.render((
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>
+), document.getElementById('root'))
